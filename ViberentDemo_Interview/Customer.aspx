f22e91bc1fa4ca85cffddccda936945da62a18cd
Nivi3535 <34634623+Nivi3535@users.noreply.github.com>
2018-07-09 23:15:58 +0930

    SolutionViberent
    Reproduced Solution

---
+++ ViberentDemo_Interview/Customer.aspx
@@ -0,0 +1,136 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Customer.aspx.cs" Inherits="ViberentDemo_Interview.Customer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:HiddenField ID="HiddenFieldId" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="lCustomerName" runat="server" Text="Customer Name"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtCustomerName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="lCustomerFN" runat="server" Text="Customer FirstName"></asp:Label>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtCustomerFN" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="lCustomerLN" runat="server" Text="CustomerLastName"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtCustomerLN" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="lCustomerPhone" runat="server" Text="Customer Contact Number"></asp:Label>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtCustomerPhone" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="lCustomerAddress" runat="server" Text="Customer Address"></asp:Label>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtCustomerAddress" runat="server" TextMode="MultiLine"></asp:TextBox>
+
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="lCustomerEmailId" runat="server" Text="Customer Email-ID"></asp:Label>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtCustomerEmailId" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="lCustomerPassword" runat="server" Text="Customer Password"></asp:Label>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtCustomerPassword" runat="server" CausesValidation="True"   ></asp:TextBox>
+                       
+                        <asp:RegularExpressionValidator ID="Regex4" runat="server" ControlToValidate="txtCustomerPassword"
+    ValidationExpression="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}"
+ErrorMessage="Password must contain: Minimum 8 characters atleast 1 UpperCase Alphabet, 1 LowerCase Alphabet, 1 Number and 1 Special Character" ForeColor="Red" />
+                        <asp:ValidationSummary ID="ValidationSummary1" runat="server" />   
+
+                    </td>   
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="loperation" runat="server" Text="Operation"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+                        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lSuccessM" runat="server" Text="Success Message" ></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Label ID="lSuccessMsg" runat="server" Text="" ForeColor="RoyalBlue"></asp:Label>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="IvalidationM" runat="server" Text="Validation Message"></asp:Label>
+
+                    </td>
+                    <td>
+                        <asp:Label ID="lValidationSummary" runat="server" Text="" ForeColor="Tomato"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <br />
+            <asp:GridView ID="GridCustomerDetails" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="CustomerId" HeaderText="ID" />
+                    <asp:BoundField DataField="CustomerName" HeaderText="Name" />
+                    <asp:BoundField DataField="CustomerFirstName" HeaderText="FirstName" />
+                    <asp:BoundField DataField="CustomerLastName" HeaderText="LastName" />
+                    <asp:BoundField DataField="CustomerPhoneNumber" HeaderText="Contact Number" />
+                    <asp:BoundField DataField="CustomerAddress" HeaderText="Address" />
+                    <asp:BoundField DataField="CustomerEmailId" HeaderText="Email Id" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="LinkButton1" runat="server" OnClick="Link_OnClick" CommandArgument='<%# Eval("CustomerId") %>'>Click Here To Update or View</asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ ViberentDemo_Interview/Customer.aspx.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace ViberentDemo_Interview
+{
+    public partial class Customer : System.Web.UI.Page
+
+    {
+        string btninput;
+        //Connection String
+        SqlConnection connection = new SqlConnection(@"Data Source = DESKTOP-ODLE4NA; Initial Catalog = ViberentCustomerCRUD ; Integrated Security = true");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnDelete.Enabled = false;
+                FillingGrid();
+            }
+
+        }
+        //Delete Operation "Deleted using stored Procedure DeleteByID 
+        //In this case record is deleted based on CustomerId(Autogenrated and hidden field)" 
+       
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+            SqlCommand command = new SqlCommand("DeleteByID", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@CustomerId", Convert.ToInt32(HiddenFieldId.Value));
+            command.ExecuteNonQuery();
+            Delete();
+
+            lSuccessMsg.Text = "Deleted Successfully !! ";
+            FillingGrid();
+
+        }
+
+        // Add or Update Based on string value btninput 
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            if (connection.State == ConnectionState.Closed)
+
+                connection.Open();
+            //Checking if btninput is Update if then it perform Update operation else Add(Insert) operation
+            if (btninput == "Update")
+            {
+               
+                SqlCommand command = new SqlCommand("CustomerUpdate", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                // command.Parameters.Add("@CustomerId", (HiddenFieldId.Value == "" ? 0 : Convert.ToInt32(HiddenFieldId.Value)));
+                command.Parameters.Add("@CustomerName", SqlDbType.NVarChar).Value = txtCustomerName.Text;
+                command.Parameters.Add("@CustomerFirstName", SqlDbType.NVarChar).Value = txtCustomerFN.Text;
+                command.Parameters.AddWithValue("@CustomerLastName", SqlDbType.NVarChar).Value = txtCustomerLN.Text;
+                command.Parameters.AddWithValue("@CustomerPhoneNumber", SqlDbType.NVarChar).Value = txtCustomerPhone.Text;
+                command.Parameters.AddWithValue("@CustomerAddress", SqlDbType.NVarChar).Value = txtCustomerAddress.Text;
+                command.Parameters.AddWithValue("@CustomerEmailId", SqlDbType.NVarChar).Value = txtCustomerEmailId.Text;
+                command.Parameters.AddWithValue("@CustomerPassword", SqlDbType.NVarChar).Value = txtCustomerPassword.Text;
+                command.ExecuteNonQuery();
+                // string CustomerID_Check = HiddenFieldId.Value;
+                // Delete();
+                connection.Close();
+                lSuccessMsg.Text = "Upated Successfully !!";
+                FillingGrid();
+            }
+           else
+            { 
+            string query = "select count(*)  from ViberentCustomer where CustomerEmailId= '" + txtCustomerEmailId.Text + "'";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            string isemail = cmd.ExecuteScalar().ToString();
+                 if(isemail == "0")
+                 {
+                        SqlCommand command = new SqlCommand("CustomerAddorUpdate", connection);
+                        command.CommandType = CommandType.StoredProcedure;
+                         command.Parameters.AddWithValue("@CustomerId", (HiddenFieldId.Value =="" ? 0 : Convert.ToInt32(HiddenFieldId.Value)));
+                         command.Parameters.AddWithValue("@CustomerName", txtCustomerName.Text);
+                        command.Parameters.AddWithValue("@CustomerFirstName", txtCustomerFN.Text);
+                        command.Parameters.AddWithValue("@CustomerLastName", txtCustomerLN.Text);
+                        command.Parameters.AddWithValue("@CustomerPhoneNumber", txtCustomerPhone.Text);
+                        command.Parameters.AddWithValue("@CustomerAddress", txtCustomerAddress.Text);
+                        command.Parameters.AddWithValue("@CustomerEmailId", txtCustomerEmailId.Text);
+                        command.Parameters.AddWithValue("@CustomerPassword", txtCustomerPassword.Text);
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                        lSuccessMsg.Text = "Saved Successfully !!";
+                        FillingGrid();
+                }
+                else
+                {
+                        lValidationSummary.Text = "Cannot perform ADD Email already exist";
+                }
+            }
+        }
+        void FillingGrid()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("CustomerView", connection);
+                sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                connection.Close();
+                GridCustomerDetails.DataSource = dataTable;
+                GridCustomerDetails.DataBind();
+                connection.Close();
+
+            }
+        }
+
+        protected void Link_OnClick(object sender, EventArgs e)
+        {
+          //  lSuccessMsg.Text = "";
+            int CustomerId = Convert.ToInt32((sender as LinkButton).CommandArgument);
+            if (connection.State == ConnectionState.Closed)
+
+                connection.Open();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("ViewByID", connection);
+            sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@CustomerId", CustomerId);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            connection.Close();
+            GridCustomerDetails.DataSource = dataTable;
+            GridCustomerDetails.DataBind();
+            HiddenFieldId.Value = CustomerId.ToString();
+            txtCustomerName.Text = dataTable.Rows[0]["CustomerName"].ToString();
+            txtCustomerFN.Text = dataTable.Rows[0]["CustomerFirstName"].ToString();
+            txtCustomerLN.Text = dataTable.Rows[0]["CustomerLastName"].ToString();
+
+            txtCustomerPhone.Text = dataTable.Rows[0]["CustomerPhoneNumber"].ToString();
+            txtCustomerAddress.Text = dataTable.Rows[0]["CustomerAddress"].ToString();
+            txtCustomerEmailId.Text = dataTable.Rows[0]["CustomerEmailId"].ToString();
+            btnAdd.Text = "Update";
+            btninput = btnAdd.Text;
+            btnDelete.Enabled = true;
+        }
+       
+            protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Delete();
+        }
+        public void Delete()
+        {
+            HiddenFieldId.Value = "";
+            txtCustomerName.Text = "";
+            txtCustomerFN.Text = "";
+            txtCustomerLN.Text = "";
+            txtCustomerPhone.Text = "";
+            txtCustomerAddress.Text = "";
+            txtCustomerEmailId.Text = "";
+            txtCustomerPassword.Text = "";
+            btnAdd.Text = "Add";
+            btnDelete.Enabled = false;
+        }
+
+
+    }
+      
+}
